cfd51c2dbf5f0a6bc6169bac9ac1d91a6921eeb1
John Doe <tt@tt.tt>
2024-08-20 19:36:04 +0200

    Gestion de la création de compte utilisateur et des erreurs utilisateur deja inscrit ou mot de passe non identique

--- shop/views.py
+++ shop/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import redirect, render
 from .models import Product, Commande
 from django.core.paginator import Paginator
 from key_generator.key_generator import generate
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.db import IntegrityError
 
 
 # Create your views here.
@@ -36,22 +39,55 @@ def checkout(request):
         ville = request.POST.get('ville')
         pays = request.POST.get('pays')
         zipcode= request.POST.get('zipcode')
-        com = Commande(items=items,total=total, nom=nom, email=email, address=address, ville=ville, pays=pays, zipcode=zipcode)
+        com = Commande(
+            items=items,
+            total=total, 
+            nom=nom, 
+            email=email, 
+            address=address, 
+            ville=ville, 
+            pays=pays, 
+            zipcode=zipcode
+        )
         com.save()
         return redirect('confirmation')
     return render(request, 'shop/checkout.html') 
 
 
 def create_account_user(request):
-    key = generate(seed = 101)
-    userName = request.POST.get('complet_name', None)
-    userPass = request.POST.get('mot_de_passe', None)
-    userId = request.POST.get('identifiant', None)
-    print(userName)
-    print(userPass)
-    print(userId)
-    print(key.get_key())
-    return render(request, 'shop/create_user.html') 
+    if request.method == "POST":
+        key = generate(seed = 101)
+        userName = request.POST.get('username', None)
+        userPass = request.POST.get('password1', None)
+        userPassConfirm = request.POST.get('password2', None)
+        print(userName)
+        print(userPass)
+        print(userPassConfirm)
+        print(key.get_key())
+        if userPass == userPassConfirm:
+            try:
+                user = User.objects.create_user(username=userName, password=userPass)
+                user.save()
+            except IntegrityError:
+                return render(
+                    request, 
+                    'shop/create_user.html', 
+                    {
+                        "form": UserCreationForm,
+                        "erreur": "Cet utilisateur est déjà inscrit"
+                    }
+                ) 
+        else:
+            return render(
+                request, 
+                'shop/create_user.html', 
+                {
+                    "form": UserCreationForm,
+                    "erreur" : "Les mots de passe ne sont pas identiques"
+                }
+            ) 
+    else:
+        return render(request, 'shop/create_user.html', {"form": UserCreationForm}) 
 
 
 
