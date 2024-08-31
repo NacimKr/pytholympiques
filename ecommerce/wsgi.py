d8e98a743c2ec10451b57bf4cedf84b8429a6f47
John Doe <tt@tt.tt>
2024-08-31 17:58:54 +0200

    Ajout des cle generer sur la creation de compte et paiement

--- ecommerce/wsgi.py
+++ ecommerce/wsgi.py
@@ -7,31 +7,13 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
-# import os
-
-# from django.core.wsgi import get_wsgi_application
-
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce.settings')
-
-# application = get_wsgi_application()
-
-
-#NEW
 import os
-import sys
 
-# add your project directory to the sys.path
-project_home = '/home/nasoooo/pytholympiques'
-if project_home not in sys.path:
- sys.path.insert(0, project_home)
+from django.core.wsgi import get_wsgi_application
 
-os.chdir(project_home)
-# set environment variable to tell django where your settings.py is
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce.settings')
 
-import django
-django.setup()
-
-# serve django via WSGI
-from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
+
+
